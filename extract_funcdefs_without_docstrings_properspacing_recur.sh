732ffbefdde49b0c428449cdba7333a72d1d1cc3
Antonio Valerio Miceli Barone <amiceli@meili.inf.ed.ac.uk>
2017-04-07 19:44:38 +0100

    reorganize 2

--- extract_funcdefs_without_docstrings_properspacing_recur.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-
-find $1 -type f -name "*.py" | python extract_funcdefs_without_docstrings_properspacing.py $2 $3
-
--- prepare_data_mono_ps.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-
-paste -d" DCNL " data_mono_ps.declarations /dev/null /dev/null /dev/null /dev/null /dev/null data_mono_ps.bodies > data_mono_ps.declbodies
-
